b4f11e0ac45d3c0b26e293dd8647a4e0c8736424
charliethegoldfish <charliefrancis.cassidy@gmail.com>
2026-01-30 15:55:26 +1100

    added more tests for text_node_to_html_node function

--- src/test_textnode_to_htmlnode.py
+++ src/test_textnode_to_htmlnode.py
@@ -10,3 +10,46 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+
+    def test_italic(self):
+        node = TextNode("This is an italic node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is an italic node")
+
+    def test_code(self):
+        node = TextNode("This is code", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is code")
+
+    def test_link(self):
+        node = TextNode("This is a link", TextType.LINK, "www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link")
+        self.assertIn("href", html_node.props)
+        self.assertEqual(html_node.props["href"], "www.google.com")
+    
+    def test_image(self):
+        text = "This is an image"
+        url = "www.google.com"
+        node = TextNode(text, TextType.IMAGE, url)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+
+        self.assertIn("src", html_node.props)
+        self.assertEqual(html_node.props["src"], url)
+
+        self.assertIn("alt", html_node.props)
+        self.assertEqual(html_node.props["alt"], text)
+    
+    
+        
