7898b3d72d44875b15264e64726f60dec4e743b6
Charlie Cassidy <charliefrancis.cassidy@gmail.com>
2026-07-12 09:12:13 +1000

    added tests for ordered and unordered lists

--- src/test_markdown_to_html.py
+++ src/test_markdown_to_html.py
@@ -17,3 +17,23 @@ the **same** even with inline stuff
 			html,
 			"<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
 		)
+
+	def test_unordered_list(self):
+		md = """
+- A list element
+- Another with **bold**
+- How about `code`
+"""
+		node = markdown_to_html_node(md)
+		html = node.to_html()
+		success = "<div><ul><li>A list element</li><li>Another with <b>bold</b></li><li>How about <code>code</code></li></ul></div>"
+
+	def test_ordered_list(self):
+		md = """
+1. A list element
+2. Another with **bold**
+3. How about `code`
+"""
+		node = markdown_to_html_node(md)
+		html = node.to_html()
+		success = "<div><ol><li>A list element</li><li>Another with <b>bold</b></li><li>How about <code>code</code></li></ol></div>"
