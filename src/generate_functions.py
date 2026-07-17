469908643585341dc3b1e999e22a3e278cf0c5b0
Charlie Cassidy <charliefrancis.cassidy@gmail.com>
2026-07-18 09:23:35 +1000

    adding recursive generate pages function

--- src/generate_functions.py
+++ src/generate_functions.py
@@ -27,4 +27,18 @@ def generate_page(from_path, template_path, dest_path):
 		f.write(template)
 	
 
-
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+	contents = os.listdir(dir_path_content)
+	for content in contents:
+		source_path = os.path.join(dir_path_content, content)
+		
+		if os.path.isfile(source_path):
+			# Make sure we're a markdown file
+			if content.endswith(".md"):
+				stripped_name = content.strip(".md")
+				new_name = stripped_name + ".html"
+				dest_path = os.path.join(dest_dir_path, new_name)
+				generate_page(source_path, template_path, dest_path)
+		else:
+			dest_path = os.path.join(dest_dir_path, content)
+			generate_pages_recursive(source_path, template_path, dest_path)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from copy_functions import copy_directory_contents
-from generate_functions import generate_page
-from config import PUBLIC_PATH, STATIC_PATH
+from generate_functions import generate_pages_recursive
+from config import PUBLIC_PATH, STATIC_PATH, CONTENT_PATH, TEMPLATE_PATH
 
 def main():
 	copy_directory_contents(STATIC_PATH, PUBLIC_PATH)
-	generate_page("content/index.md", "template.html", "public/index.html")
+	generate_pages_recursive(CONTENT_PATH, TEMPLATE_PATH, PUBLIC_PATH)
 
 
 main()
